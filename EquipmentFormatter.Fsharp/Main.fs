2de1e702a3b248ba1e7a5e86344ee24ff8c1b494
Romain Deneau (SOAT) <romain.deneau-ext@aldautomotive.com>
2020-04-27 22:06:55 +0200

    Operation with NoOp

--- EquipmentFormatter.Fsharp/Main.fs
+++ EquipmentFormatter.Fsharp/Main.fs
@@ -2,58 +2,66 @@ module EquipmentMapper
 
 open EquipmentFormatter.Fsharp.External
 
-let computeLabelTotallyNew (variation: Variation) =
-    match variation.Schema with
-    | 7407 -> Some "Nombre de cylindres"
-    | 15304 -> Some "Puissance (ch)"
-    | 15305 -> Some "Régime de puissance maxi (tr/mn)"
-    | _ -> None
+type Operation =
+    | Exchange of other: string
+    | Replacement of part: string * by: string
+    | Supplement of suffix: string
+    | NoOp
 
-let computeLabelWithReplacementBySchema (label: string) (variation: Variation) =
-    match variation.Schema with
-    | 23502
-    | 24002 -> Some(label.Replace("an(s) / km", ": durée (ans)"))
-    | 23503
-    | 24003 -> Some(label.Replace("an(s) / km", ": kilométrage"))
-    | 7403 -> Some(label.Replace("litres / cm3", "litres"))
-    | 7402 -> Some(label.Replace("litres / cm3", "cm3"))
-    | _ -> None
+let proceed (label: string) operation =
+    match operation with
+    | Exchange other -> other
+    | Replacement(part, by) -> label.Replace(part, by)
+    | Supplement suffix -> label + suffix
+    | NoOp -> label
 
-let computeLabelWithReplacementByLocation (label: string) (variation: Variation) =
+let tryMapToExchange (variation: Variation) =
+    match variation.Schema with
+    | 7407  -> Exchange "Nombre de cylindres"
+    | 15304 -> Exchange "Puissance (ch)"
+    | 15305 -> Exchange "Régime de puissance maxi (tr/mn)"
+    | _ -> NoOp
+
+let tryMapToReplacementBySchema (variation: Variation) =
+    match variation.Schema with
+    | 23502 | 24002 -> Replacement("an(s) / km", ": durée (ans)")
+    | 23503 | 24003 -> Replacement("an(s) / km", ": kilométrage")
+    | 7403 -> Replacement("litres / cm3", "litres")
+    | 7402 -> Replacement("litres / cm3", "cm3")
+    | _ -> NoOp
+
+let tryMapToReplacementByLocation (variation: Variation) =
     match variation.Schema, variation.Location with
-    | 23301, 'F' -> Some(label.Replace("AV / AR", "AV"))
-    | 23301, 'R' -> Some(label.Replace("AV / AR", "AR"))
-    | 17811, 'D'
-    | 17818, 'D' -> Some(label.Replace("conducteur / passager", "conducteur"))
-    | 17811, 'P'
-    | 17818, 'P' -> Some(label.Replace("conducteur / passager", "passager"))
-    | 53405, 'D' -> Some(label.Replace("recharge (rapide) A / V / h", "recharge : ampérage (A)"))
-    | 53405, 'F' -> Some(label.Replace("recharge (rapide) A / V / h", "recharge rapide : ampérage (A)"))
-    | 53404, 'D' -> Some(label.Replace("recharge (rapide) A / V / h", "recharge : voltage (V)"))
-    | 53404, 'F' -> Some(label.Replace("recharge (rapide) A / V / h", "recharge rapide : voltage (V)"))
-    | 53403, 'D' -> Some(label.Replace("recharge (rapide) A / V / h", "recharge : durée (heures)"))
-    | 53403, 'F' -> Some(label.Replace("recharge (rapide) A / V / h", "recharge rapide : durée (heures)"))
-    | _ -> None
+    | 17811, 'D' | 17818, 'D' -> Replacement("conducteur / passager", "conducteur")
+    | 17811, 'P' | 17818, 'P' -> Replacement("conducteur / passager", "passager")
+    | 53405, 'D' -> Replacement("recharge (rapide) A / V / h", "recharge : ampérage (A)")
+    | 53405, 'F' -> Replacement("recharge (rapide) A / V / h", "recharge rapide : ampérage (A)")
+    | 53404, 'D' -> Replacement("recharge (rapide) A / V / h", "recharge : voltage (V)")
+    | 53404, 'F' -> Replacement("recharge (rapide) A / V / h", "recharge rapide : voltage (V)")
+    | 53403, 'D' -> Replacement("recharge (rapide) A / V / h", "recharge : durée (heures)")
+    | 53403, 'F' -> Replacement("recharge (rapide) A / V / h", "recharge rapide : durée (heures)")
+    | 23301, 'F' -> Replacement("AV / AR", "AV")
+    | 23301, 'R' -> Replacement("AV / AR", "AR")
+    | _ -> NoOp
 
-let computeLabelWithComplementaryInfo (label: string) (variation: Variation) =
+let tryMapToSupplement (variation: Variation) =
     match variation.Schema with
-    | 14103 -> Some(label + " : largeur")
-    | 14104 -> Some(label + " : profil")
-    | _ -> None
+    | 14103 -> Supplement " : largeur"
+    | 14104 -> Supplement " : profil"
+    | _ -> NoOp
 
-let (<||>) fa fb =
-    fun x ->
-        match fa x with
-        | Some v -> Some v
-        | None -> fb x
+let (<||>) fa fb x =
+    match fa x with
+    | NoOp -> fb x
+    | op -> op
+
+let mapToOperation =
+    tryMapToExchange
+    <||> tryMapToReplacementBySchema
+    <||> tryMapToReplacementByLocation
+    <||> tryMapToSupplement
 
 let computeLabel (equipment: Equipment) (variation: Variation) =
-    let label = equipment.Label.Trim()
-    let computeLabel' =
-        computeLabelTotallyNew
-        <||> computeLabelWithReplacementBySchema label
-        <||> computeLabelWithReplacementByLocation label
-        <||> computeLabelWithComplementaryInfo label
     variation
-    |> computeLabel'
-    |> (fun x -> defaultArg x label)
+    |> mapToOperation
+    |> proceed (equipment.Label.Trim())
