dae36ac726e2691524bc4f8146f9e28f179e653c
Romain Deneau (SOAT) <romain.deneau-ext@aldautomotive.com>
2020-04-28 06:54:11 +0200

    Operation without NoOp

--- EquipmentFormatter.Fsharp/Main.fs
+++ EquipmentFormatter.Fsharp/Main.fs
@@ -6,54 +6,52 @@ type Operation =
     | Exchange of other: string
     | Replacement of part: string * by: string
     | Supplement of suffix: string
-    | NoOp
 
 let proceed (label: string) operation =
     match operation with
     | Exchange other -> other
-    | Replacement(part, by) -> label.Replace(part, by)
+    | Replacement (part, by) -> label.Replace(part, by)
     | Supplement suffix -> label + suffix
-    | NoOp -> label
 
 let tryMapToExchange (variation: Variation) =
     match variation.Schema with
-    | 7407  -> Exchange "Nombre de cylindres"
-    | 15304 -> Exchange "Puissance (ch)"
-    | 15305 -> Exchange "Régime de puissance maxi (tr/mn)"
-    | _ -> NoOp
+    | 7407  -> Some (Exchange "Nombre de cylindres")
+    | 15304 -> Some (Exchange "Puissance (ch)")
+    | 15305 -> Some (Exchange "Régime de puissance maxi (tr/mn)")
+    | _ -> None
 
 let tryMapToReplacementBySchema (variation: Variation) =
     match variation.Schema with
-    | 23502 | 24002 -> Replacement("an(s) / km", ": durée (ans)")
-    | 23503 | 24003 -> Replacement("an(s) / km", ": kilométrage")
-    | 7403 -> Replacement("litres / cm3", "litres")
-    | 7402 -> Replacement("litres / cm3", "cm3")
-    | _ -> NoOp
+    | 23502 | 24002 -> Some (Replacement("an(s) / km", ": durée (ans)"))
+    | 23503 | 24003 -> Some (Replacement("an(s) / km", ": kilométrage"))
+    | 7403 -> Some (Replacement("litres / cm3", "litres"))
+    | 7402 -> Some (Replacement("litres / cm3", "cm3"))
+    | _ -> None
 
 let tryMapToReplacementByLocation (variation: Variation) =
     match variation.Schema, variation.Location with
-    | 17811, 'D' | 17818, 'D' -> Replacement("conducteur / passager", "conducteur")
-    | 17811, 'P' | 17818, 'P' -> Replacement("conducteur / passager", "passager")
-    | 53405, 'D' -> Replacement("recharge (rapide) A / V / h", "recharge : ampérage (A)")
-    | 53405, 'F' -> Replacement("recharge (rapide) A / V / h", "recharge rapide : ampérage (A)")
-    | 53404, 'D' -> Replacement("recharge (rapide) A / V / h", "recharge : voltage (V)")
-    | 53404, 'F' -> Replacement("recharge (rapide) A / V / h", "recharge rapide : voltage (V)")
-    | 53403, 'D' -> Replacement("recharge (rapide) A / V / h", "recharge : durée (heures)")
-    | 53403, 'F' -> Replacement("recharge (rapide) A / V / h", "recharge rapide : durée (heures)")
-    | 23301, 'F' -> Replacement("AV / AR", "AV")
-    | 23301, 'R' -> Replacement("AV / AR", "AR")
-    | _ -> NoOp
+    | 17811, 'D' | 17818, 'D' -> Some (Replacement("conducteur / passager", "conducteur"))
+    | 17811, 'P' | 17818, 'P' -> Some (Replacement("conducteur / passager", "passager"))
+    | 53405, 'D' -> Some (Replacement("recharge (rapide) A / V / h", "recharge : ampérage (A)"))
+    | 53405, 'F' -> Some (Replacement("recharge (rapide) A / V / h", "recharge rapide : ampérage (A)"))
+    | 53404, 'D' -> Some (Replacement("recharge (rapide) A / V / h", "recharge : voltage (V)"))
+    | 53404, 'F' -> Some (Replacement("recharge (rapide) A / V / h", "recharge rapide : voltage (V)"))
+    | 53403, 'D' -> Some (Replacement("recharge (rapide) A / V / h", "recharge : durée (heures)"))
+    | 53403, 'F' -> Some (Replacement("recharge (rapide) A / V / h", "recharge rapide : durée (heures)"))
+    | 23301, 'F' -> Some (Replacement("AV / AR", "AV"))
+    | 23301, 'R' -> Some (Replacement("AV / AR", "AR"))
+    | _ -> None
 
 let tryMapToSupplement (variation: Variation) =
     match variation.Schema with
-    | 14103 -> Supplement " : largeur"
-    | 14104 -> Supplement " : profil"
-    | _ -> NoOp
+    | 14103 -> Some (Supplement " : largeur")
+    | 14104 -> Some (Supplement " : profil")
+    | _ -> None
 
 let (<||>) fa fb x =
     match fa x with
-    | NoOp -> fb x
-    | op -> op
+    | Some v -> Some v
+    | None -> fb x
 
 let mapToOperation =
     tryMapToExchange
@@ -62,6 +60,9 @@ let mapToOperation =
     <||> tryMapToSupplement
 
 let computeLabel (equipment: Equipment) (variation: Variation) =
+    let label = equipment.Label.Trim()
+
     variation
     |> mapToOperation
-    |> proceed (equipment.Label.Trim())
+    |> Option.map (proceed label)
+    |> (fun x -> defaultArg x label)
