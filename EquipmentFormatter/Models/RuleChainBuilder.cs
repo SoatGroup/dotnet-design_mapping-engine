669c1b292c383c7d26eacc9a5c28cfef1d42b66f
Romain Deneau (SOAT) <romain.deneau-ext@aldautomotive.com>
2020-05-24 10:12:11 +0200

    builder mutable with interfaces outside

--- EquipmentFormatter/Models/RuleChainBuilder.cs
+++ EquipmentFormatter/Models/RuleChainBuilder.cs
@@ -1,65 +1,64 @@
 using System;
-using System.Collections.Immutable;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace EquipmentFormatter.Models
 {
   public static class RuleChainBuilder
   {
-    public static RuleChainBuilder<TCriteria, TData>.ITakingCondition Given<TCriteria, TData>(TCriteria criteria, TData data) =>
-      new RuleChainBuilder<TCriteria, TData>.TakingConditionOrEnding(criteria, data, ImmutableStack<Rule<TCriteria, TData>>.Empty);
+    public static ITakingCondition<TCriteria, TData> Given<TCriteria, TData>(TCriteria criteria, TData data) =>
+      new RuleChainBuilder<TCriteria, TData>(criteria, data);
   }
 
-  public static class RuleChainBuilder<TCriteria, TData>
+  public interface ITakingCondition<TCriteria, TData>
   {
-    public interface ITakingCondition
+    ITakingOperation<TCriteria, TData> When(Condition<TCriteria> condition);
+  }
+
+  public interface ITakingConditionOrEnding<TCriteria, TData> : ITakingCondition<TCriteria, TData>
+  {
+    TData Else(Func<TData, TData> operation);
+  }
+
+  public interface ITakingOperation<TCriteria, TData>
+  {
+    ITakingConditionOrEnding<TCriteria, TData> Then(Func<TData, TData> operation);
+  }
+
+  public sealed class RuleChainBuilder<TCriteria, TData> :
+    ITakingConditionOrEnding<TCriteria, TData>,
+    ITakingOperation<TCriteria, TData>
+  {
+    private TCriteria Criteria { get; }
+    private TData Data { get; }
+    private Stack<Rule<TCriteria, TData>> Rules { get; } = new Stack<Rule<TCriteria, TData>>();
+    private Condition<TCriteria> Condition { get; set; }
+
+    public RuleChainBuilder(TCriteria criteria, TData data)
     {
-      TakingOperation When(Condition<TCriteria> condition);
+      Criteria = criteria;
+      Data     = data;
     }
 
-    public sealed class TakingConditionOrEnding : ITakingCondition
+    public ITakingOperation<TCriteria, TData> When(Condition<TCriteria> condition)
     {
-      private TCriteria Criteria { get; }
-      private TData Data { get; }
-      private ImmutableStack<Rule<TCriteria, TData>> Rules { get; }
-
-      public TakingConditionOrEnding(TCriteria criteria, TData data, ImmutableStack<Rule<TCriteria, TData>> rules)
-      {
-        Criteria = criteria;
-        Data     = data;
-        Rules    = rules;
-      }
-
-      public TakingOperation When(Condition<TCriteria> condition) =>
-        new TakingOperation(condition, Criteria, Data, Rules);
-
-      public TData Else(Func<TData, TData> operation) =>
-        Rules.Aggregate(seed: EndRuleChainWith(operation),
-                        (chain, rule) => new RuleChain<TCriteria, TData>(rule, chain))
-             .SelectOperationAdaptedTo(Criteria)
-             .ApplyOn(Data);
-
-      private static RuleChain<TCriteria, TData> EndRuleChainWith(Func<TData, TData> operation) =>
-        new RuleChain<TCriteria, TData>(Rule<TCriteria, TData>.Default(operation), null);
+      Condition = condition;
+      return this;
     }
 
-    public sealed class TakingOperation
+    public ITakingConditionOrEnding<TCriteria, TData> Then(Func<TData, TData> operation)
     {
-      private Condition<TCriteria> Condition { get; }
-      private TCriteria Criteria { get; }
-      private TData Data { get; }
-      private ImmutableStack<Rule<TCriteria, TData>> Rules { get; }
-
-      public TakingOperation(Condition<TCriteria> condition, TCriteria criteria, TData data, ImmutableStack<Rule<TCriteria, TData>> rules)
-      {
-        Condition = condition;
-        Criteria  = criteria;
-        Data      = data;
-        Rules     = rules;
-      }
-
-      public TakingConditionOrEnding Then(Func<TData, TData> operation) =>
-        new TakingConditionOrEnding(Criteria, Data, Rules.Push(new Rule<TCriteria, TData>(Condition, operation)));
+      Rules.Push(new Rule<TCriteria, TData>(Condition, operation));
+      return this;
     }
+
+    public TData Else(Func<TData, TData> operation) =>
+      Rules.Aggregate(seed: EndRuleChainWith(operation),
+                      (chain, rule) => new RuleChain<TCriteria, TData>(rule, chain))
+           .SelectOperationAdaptedTo(Criteria)
+           .ApplyOn(Data);
+
+    private static RuleChain<TCriteria, TData> EndRuleChainWith(Func<TData, TData> operation) =>
+      new RuleChain<TCriteria, TData>(Rule<TCriteria, TData>.Default(operation), null);
   }
 }
